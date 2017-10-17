4600974efb73fdc6cab0f6e09c9defc74dd1a4ed
Dreladus <sejuapig@gmail.com>
2017-10-17 22:39:08 +0200

    Ajout JavaDoc package Articles

--- Miniprojet/articles/MatelasAir.java
+++ Miniprojet/articles/MatelasAir.java
@@ -1,6 +1,7 @@
 package articles;
 
 /**
+ * Classe permettant de modéliser l'article MatelasAir.
  * Created by E149769S on 19/09/17.
  */
 public class MatelasAir extends MobilierMedical {
@@ -8,6 +9,14 @@ public class MatelasAir extends MobilierMedical {
     private double poidsMax;
     private double tempsGonflage;
 
+    /**
+     * Constructor
+     * @param reference - La référence de l'article
+     * @param marque - La marque de l'article
+     * @param modele - Le modèle de l'article
+     * @param prix_j - Le prix par jour de l'article
+     * @param nbDispo - Le nombre d'article disponnibles
+     */
     public MatelasAir(String reference, String marque, String modele, double prix_j, int nbDispo) {
         super(reference, marque, modele, prix_j, nbDispo);
     }
--- Miniprojet/articles/SouleveMalade.java
+++ Miniprojet/articles/SouleveMalade.java
@@ -1,12 +1,21 @@
 package articles;
 
 /**
+ * Classe permettant de modéliser l'article SouleveMalade.
  * Created by E149769S on 19/09/17.
  */
 public class SouleveMalade extends Article {
     private double capacite;
     private double angle;
 
+    /**
+     * Constructor
+     * @param reference - La référence de l'article
+     * @param marque - La marque de l'article
+     * @param modele - Le modèle de l'article
+     * @param prix_j - Le prix par jour de l'article
+     * @param nbDispo - Le nombre d'article disponnibles
+     */
     public SouleveMalade(String reference, String marque, String modele, double prix_j, int nbDispo) {
         super(reference, marque, modele, prix_j, nbDispo);
     }
